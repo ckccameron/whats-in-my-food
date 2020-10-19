72523b3166af5e55af5521b6f8503bfaf1c2f5fa
Cam C <ckc.cameron@gmail.com>
2020-10-19 09:00:54 -0700

    Build start to how SearchFacade will create food objects

---
+++ app/facades/search_facade.rb
@@ -0,0 +1,15 @@
+class SearchFacade
+  def self.foods_search(query)
+    results = service.get_foods_results(query)
+    results.map { |food_result_info| food(food_result_info) }
+  end
+
+  def self.service
+    FoodService.new
+  end
+
+  def self.food(food_result_info)
+    Food.new(food_result_info)
+  end
+
+end
