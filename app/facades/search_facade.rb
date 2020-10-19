ece2cab6187b2e41086b132870ab2cb7f5d9a7ab
Cam C <ckc.cameron@gmail.com>
2020-10-19 09:43:55 -0700

    Fix foods_search method within SearchFacade
    * realign data structure to have access to keys I want to create food 
    attributes

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,6 +1,6 @@
 class SearchFacade
   def self.foods_search(query)
-    results = service.get_foods_results(query)
+    results = service.get_foods_results(query)[:foods]
     results.map { |food_result_info| food(food_result_info) }
   end
 
